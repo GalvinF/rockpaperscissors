213a7f59c166c764d774a9d0b43222b591ad0b9e
Glitch (gf-rock-paper-scissors-game) <none>
2022-01-13 02:04:34 +0000

    ⛓️📪 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,50 +1,56 @@
 let randomNumber = 0;
 $(".play").click(function () {
-   // display user choice
+  // display user choice
   let userChoice = $(".input").val();
-  $(".userChoice").text(userInput);
+  $(".userChoice").text(userChoice);
   $(".input").val("");
-  
+
   // display computer choice
   let computerChoice = getRandomComputerChoice();
   $(".computerChoice").text(computerChoice);
-  
+
   // display winner
-  $(".result").text(chooseWinner(userInput, computerChoice));
+  let winner = chooseWinner(userChoice, computerChoice);
+  $(".result").text(winner);
   $(".input").text();
-  
+
   // computer choice
-function getRandomComputerChoice() {
-  randomNumber = Math.random() * 3;
-  randomNumber = Math.ceil(randomNumber);
-  // assigning rock paper and scissors to rand gen numbers 1,2, or 3
-  let computerChoice;
-  if (randomNumber === 1) {
-    computerChoice = "rock";
-  } else if (randomNumber === 2) {
-    computerChoice = "paper";
-  } else {
-    computerChoice = "scissors";
+  function getRandomComputerChoice() {
+    randomNumber = Math.random() * 3;
+    randomNumber = Math.ceil(randomNumber);
+    // assigning rock paper and scissors to rand gen numbers 1,2, or 3
+    let computerChoice;
+    if (randomNumber === 1) {
+      computerChoice = "rock";
+    } else if (randomNumber === 2) {
+      computerChoice = "paper";
+    } else {
+      computerChoice = "scissors";
+    }
+    return computerChoice;
   }
-  return computerChoice
-}
-    
-function chooseWinner(userInput, computerChoice) {
-  let winner;
-  if (
-    (userInput === "rock" && computerChoice === "paper") ||
-    (userInput === "paper" && computerChoice === "scissors") ||
-    (userInput === "scissors" && computerChoice === "rock")
-  ) {
-    winner = "Computer Wins!";
-  } else if (
-    (userInput === "rock" && computerChoice === "scissors") ||
-    (userInput === "paper" && computerChoice === "rock") ||
-    (userInput === "scissors" && computerChoice === "paper")
-  ) {
-    winner = "User Wins!";
-  } else {
-    winner = "No one Wins!";
+
+  // choose winner of game
+  function chooseWinner(userChoice, computerChoice) {
+    let winner;
+    if (
+      (userChoice === "rock" && computerChoice === "paper") ||
+      (userChoice === "paper" && computerChoice === "scissors") ||
+      (userChoice === "scissors" && computerChoice === "rock")
+    ) {
+      winner = "Computer Wins!";
+    } else if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+      winner = "User Wins!";
+    } else if (
+      (userChoice === "rock" && computerChoice === "rock") ||
+      (userChoice === "paper" && computerChoice === "paper") ||
+      (userChoice === "scissors" && computerChoice === "scissors")
+    ) {
+      winner = "Tie Game!";
+    } else winner = "Error, Try Again";
   }
-  return winner;
-}
+});
