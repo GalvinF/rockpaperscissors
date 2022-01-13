49bd12fdc6e3fcf31633cceda1844e1863ca4d14
Glitch (gf-rock-paper-scissors-game) <none>
2022-01-13 01:53:39 +0000

    🥧🗺️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 let randomNumber = 0;
 $(".play").click(function () {
-  function getRandomComputerChoice() {
-  // randomly generate computer's decision
+   // display user choice
+  let userChoice = $(".input").val();
+  $(".userChoice").text(userInput);
+  $(".input").val("");
+  
+  // display computer choice
+  let computerChoice = getRandomComputerChoice();
+  $(".computerChoice").text(computerChoice);
+  
+  // display winner
+  $(".result").text(chooseWinner(userInput, computerChoice));
+  $(".input").text();
+  
+  // computer choice
+function getRandomComputerChoice() {
   randomNumber = Math.random() * 3;
   randomNumber = Math.ceil(randomNumber);
   // assigning rock paper and scissors to rand gen numbers 1,2, or 3
@@ -10,22 +23,13 @@ $(".play").click(function () {
     computerChoice = "rock";
   } else if (randomNumber === 2) {
     computerChoice = "paper";
-  } else if (randomNumber === 3)
+  } else {
     computerChoice = "scissors";
+  }
   return computerChoice
 }
-  
-  // user choice
-  let userInput = $(".input").val();
-  $(".userChoice").text(userInput);
-  $(".input").val("");
-  
-  // computer choice
-  let computerChoice = getRandomComputerChoice();
-  $(".computerChoice").text(computerChoice);
-   
-  
-  // deciding winner
+    
+function chooseWinner(userInput, computerChoice) {
   let winner;
   if (
     (userInput === "rock" && computerChoice === "paper") ||
@@ -33,23 +37,14 @@ $(".play").click(function () {
     (userInput === "scissors" && computerChoice === "rock")
   ) {
     winner = "Computer Wins!";
-    $(".result").text(winner);
   } else if (
     (userInput === "rock" && computerChoice === "scissors") ||
     (userInput === "paper" && computerChoice === "rock") ||
     (userInput === "scissors" && computerChoice === "paper")
   ) {
-    winner = "You Win!";
-    $(".result").text(winner);
-  } else if (
-    (userInput === "rock" && computerChoice === "rock") ||
-    (userInput === "paper" && computerChoice === "paper") ||
-    (userInput === "scissors" && computerChoice === "scissors")
-  ) {
-    winner = "Tie Game!";
-    $(".result").text(winner);
+    winner = "User Wins!";
   } else {
-    winner = "Error - Try Again!"
-    $(".result").text(winner);
+    winner = "No one Wins!";
   }
-});
+  return winner;
+}
